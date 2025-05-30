806e36636fa6cfba6acd6fef38a7adc1fd18abcb
NSHkr <ZeroTrust@NSHkr.com>
2025-05-29 18:01:10 -1000

    Initial commit: Setup for ElixirScope.AST.Structures library and DESIGN.md

---
+++ lib/elixir_scope/ast/structures.ex
@@ -0,0 +1,18 @@
+defmodule ElixirScope.AST.Structures do
+  @moduledoc """
+  Documentation for `ElixirScope.AST.Structures`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ElixirScope.AST.Structures.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/elixir_scope/ast/structures_test.exs
@@ -0,0 +1,8 @@
+defmodule ElixirScope.AST.StructuresTest do
+  use ExUnit.Case
+  doctest ElixirScope.AST.Structures
+
+  test "greets the world" do
+    assert ElixirScope.AST.Structures.hello() == :world
+  end
+end
